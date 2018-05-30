ddd5ceb335bdc4ea9cf7650a5728a25428a85903
TheDraxes <tim.vinzing@web.de>
2018-05-30 18:54:18 +0200

    Erste GUI Arbeiten und das Grundgerüst des Projektes erstellt

--- src/GUI/StartGUI/StartController.java
+++ src/GUI/StartGUI/StartController.java
@@ -1,4 +1,37 @@
 package GUI.StartGUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class StartController {
+
+    @FXML
+    private ComboBox<String> InventarBox;
+
+    @FXML
+    private Button newButton;
+
+    @FXML
+    private Button ConfirmButton;
+
+    @FXML
+    void ComboBoxInit() {
+        ObservableList<String> _default = FXCollections.observableArrayList("Option 1", "Option 2");
+        InventarBox.setItems(_default);
+    }
+
+    @FXML
+    void newClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmClicked(ActionEvent event) {
+
+    }
+
 }
