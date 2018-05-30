6c185285f8af0c7f8d8e8859db3dc6474fc9dc35
TheDraxes <tim.vinzing@web.de>
2018-05-30 20:09:14 +0200

    Logik um auf das ScrollPane zuzugreifen getestet

--- src/GUI/ViewGUI/ViewController.java
+++ src/GUI/ViewGUI/ViewController.java
@@ -1,4 +1,20 @@
 package GUI.ViewGUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class ViewController {
+
+    @FXML
+    private ScrollPane ItemScrollPane;
+
+    @FXML
+    void ShowClicked(ActionEvent event) {
+        VBox test = new VBox();
+        test.getChildren().addAll(new Text("Test"));
+        ItemScrollPane.setContent(test);
+    }
 }
