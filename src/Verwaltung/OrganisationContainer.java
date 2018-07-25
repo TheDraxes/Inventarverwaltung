09a9f777589cd36b85ec461683ad52f93bb5e88f
Tim Vinzing <t.vinzing@dvz-mv.de>
2018-07-25 15:38:02 +0200

    Skelett methode für Organisation anlegen

--- src/Verwaltung/OrganisationContainer.java
+++ src/Verwaltung/OrganisationContainer.java
@@ -1,6 +1,8 @@
 package Verwaltung;
 
 import Data.Abteilung;
+import Data.Asset;
+import Data.Sachgebiet;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,5 +17,17 @@ import java.util.ArrayList;
 
 public class OrganisationContainer implements Serializable {
     private ArrayList<Abteilung> AbteilungsList = new ArrayList<Abteilung>();
+    private ArrayList<Sachgebiet> SachgebietsList = new ArrayList<>();
+
+    // neues Asset einfügen, Inventarnummer wird automatisch generiert (hochgezählt)
+    public boolean insertOrganisation(Asset a) {
+        if(true){
+            System.out.println("[INFO] Organisation angelegt");
+            return true;
+        } else {
+            System.out.println("[WARNING] Fehler");
+            return false;
+        }
+    }
 
 }
